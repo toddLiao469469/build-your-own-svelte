8eb59ca6f11d24c594c57c20334c579785da242e
ToddLiao <toddliao.dev@gmail.com>
2024-07-19 01:02:43 +0800

    feat: add analyze

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import * as fs from "fs";
 import * as acorn from "acorn";
+import * as periscopic from "periscopic";
+import * as estreewalker from "estree-walker";
 
 const content = fs.readFileSync("./app.svelte", "utf-8");
 const ast = parse(content);
-JSON.stringify(ast, null, 2);
-// const analysis = analyze(ast);
+const analysis = analyze(ast);
+console.log(analysis);
 // const js = generate(ast, analysis);
 
-fs.writeFileSync("./app.js", JSON.stringify(ast, null, 2), "utf-8");
+// fs.writeFileSync("./app.js", JSON.stringify(ast, null, 2), "utf-8");
 
 function parse(content) {
   let i = 0;
@@ -109,7 +111,6 @@ function parse(content) {
   function match(str) {
     return content.slice(i, i + str.length) === str;
   }
-  
   function eat(str) {
     if (match(str)) {
       i += str.length;
@@ -119,7 +120,7 @@ function parse(content) {
   }
   function readWhileMatching(regex) {
     let startIndex = i;
-    while (regex.test(content[i])) {
+    while (i < content.length && regex.test(content[i])) {
       i++;
     }
     return content.slice(startIndex, i);
@@ -129,5 +130,56 @@ function parse(content) {
   }
 }
 
-function analyze(ast) {}
+function analyze(ast) {
+  const result = {
+    variables: new Set(),
+    willChange: new Set(),
+    willUseInTemplate: new Set(),
+  };
+
+  const { scope: rootScope, map } = periscopic.analyze(ast.script);
+  result.variables = new Set(rootScope.declarations.keys());
+  result.rootScope = rootScope;
+  result.map = map;
+
+  let currentScope = rootScope;
+  estreewalker.walk(ast.script, {
+    enter(node) {
+      if (map.has(node)) {
+        currentScope = map.get(node);
+      }
+
+      if (
+        node.type === "UpdateExpression" &&
+        currentScope.find_owner(node.argument.name) === rootScope
+      ) {
+        result.willChange.add(node.argument.name);
+      }
+    },
+    leave(node) {
+      if (map.has(node)) {
+        currentScope = currentScope.parent;
+      }
+    },
+  });
+
+  function traverse(fragment) {
+    switch (fragment.type) {
+      case "Element":
+        fragment.attributes.forEach(traverse);
+        fragment.children.forEach(traverse);
+        break;
+      case "Attribute":
+        result.willUseInTemplate.add(fragment.value.name);
+        break;
+      case "Expression":
+        result.willUseInTemplate.add(fragment.expression.name);
+        break;
+    }
+  }
+
+  ast.html.forEach(traverse);
+
+  return result;
+}
 function generate(ast, analysis) {}
